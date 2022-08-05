735511fc9993d58270d86f32eb586bf887fb0499
KenielDev <103508290+KenielDev@users.noreply.github.com>
2022-08-05 17:03:00 -0300

    consumindo api

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 // import { Container } from './styles';
 
 const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState([]);
+
   useEffect(() => {
     getPokemons();
   }, []);
@@ -9,18 +11,41 @@ const App: React.FC = () => {
   function getPokemons() {
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154")
       .then((response) => response.json())
-      .then((result) =>
-        console.log(
-          result.results.forEach((element: any) => {
-            console.log(element.name);
-          })
-        )
-      )
-      .catch((error) => console.log("error", error));
+      .then((result) => setPokemons(result.results));
+    console.log(pokemons);
   }
+
   return (
     <>
-      <div></div>
+      <div>
+        <h1>Pokemons</h1>
+        <div>
+          {pokemons.map((pokemon) => {
+            const numbers = Object.keys(new Array(1).fill(null)).map(Number);
+
+            const selecaoPokemons = numbers.slice(1);
+
+            console.log(selecaoPokemons);
+
+            const urlPokemonsImg =
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+            const urlPokemons = `${urlPokemonsImg}${pokemon.name}/sprites`;
+
+            // for (let i = 0; i <= 1154; i++) {
+            //   const element = i;
+            //   console.log(element);
+            // }
+            return (
+              <div key={pokemon.name}>
+                <div>{pokemon.name}</div>
+                <div>
+                  <img src={`${urlPokemonsImg}/4.png`} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
