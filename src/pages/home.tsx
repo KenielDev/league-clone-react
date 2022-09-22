b989af3bb8f5eb246960b9f4efae0ef69677209c
KenielDev <kenielalves3@gmail.com>
2022-09-22 11:52:36 -0300

    hidden home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import background from "/src/assets/img/home/pant-bg.jpg";
+// import background from "/src/assets/img/home/pant-bg.jpg";
 
-export function HomeContent() {
-    return (
-        <div
-            style={{
-                backgroundImage: `url(${background})`,
-                backgroundSize: "cover",
-            }}
-            className="flex flex-col w-full h-[100vh] "
-        ></div>
-    );
-}
+// export function HomeContent() {
+//     return (
+//         <div
+//             style={{
+//                 backgroundImage: `url(${background})`,
+//                 backgroundSize: "cover",
+//             }}
+//             className="flex flex-col w-full h-[100vh] "
+//         ></div>
+//     );
+// }
